123a2dfe66309f47e5e4ea01591127bd379d6e72
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-07-15 11:20:28 -0400

    view count increments w throttling to avoid refresh spam

--- frontend/src/components/RoutinePage/FavouriteArea/FavouriteArea.tsx
+++ frontend/src/components/RoutinePage/FavouriteArea/FavouriteArea.tsx
@@ -2,15 +2,12 @@
 
 import { useState } from 'react'
 import FavouriteButton from '@/components/RoutinePage/FavouriteButton/FavouriteButton'
-import Meta from '@/components/RoutinePage/Meta/Meta'
 
 export default function FavouriteArea({
   id,
-  initialViews,
   initialFavourites
 }: {
   id: string
-  initialViews: number | null
   initialFavourites: number | null
 }) {
   const [favourites, setFavourites] = useState(initialFavourites ?? 0)
@@ -22,7 +19,9 @@ export default function FavouriteArea({
         onFavouritesChange={delta => setFavourites(f => f + delta)}
       />
 
-      <Meta id={id} views={initialViews} favourites={favourites} />
+      <p>
+        {favourites} favorites&nbsp;·&nbsp;
+      </p>
     </>
   )
 }
